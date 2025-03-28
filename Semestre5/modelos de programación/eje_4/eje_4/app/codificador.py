f794981541ba8db051872507096a204a08af8e82
melqui romero <melquiromero@iMac-de-melqui.local>
2025-03-28 16:10:44 -0500

    depuración archivos

--- Semestre5/modelos de programación/eje_4/eje_4/app/codificador.py
+++
@@ -1,5 +0,0 @@
-def formatear_mensaje(mensaje):
-    return mensaje.encode('utf-8')
-
-def decodificar_mensaje(mensaje):
-    return mensaje.decode('utf-8')
--- Semestre5/modelos de programación/eje_4/eje_4/app/preguntas.py
+++
@@ -1,7 +0,0 @@
-PREGUNTAS = [
-    "¿Cuál es la raíz del árbol?",
-    "¿Cuántos nodos hay en total?",
-    "¿Cuál es el recorrido en orden?",
-    "¿Cuál es la altura del árbol?",
-    "¿Cuál es el nodo mayor y el menor?"
-]
